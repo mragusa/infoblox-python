de0ea35b1c050ee37992f6694aed2f6eb4099aa3
Michael Ragusa <mragusa@infoblox.com>
2024-05-16 01:48:23 -0400

    replaced click with argparse. fixed typing to allow query ID match

--- dns-splitter.py
+++ dns-splitter.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
 from scapy.all import *
-import click
+import argparse 
 
-@click.command()
-@click.option("-p", "--pcap", help="path to PCAP file")
-@click.option("-i", "--id", help="DNS ID to filter on")
+def write(pkt, dnsid):
+    wrpcap(dnsid + '.pcap', pkt, append=True)  #appends packet to output file
 
-def write:
-    wrpcap(id + '.pcap', pkt, append=True)  #appends packet to output file
-
-def splitpcap(pcap):
+def splitpcap(pcap, dnsid):
+    print("Opening file: {}".format(pcap))
+    print("Searching for Query ID: {}".format(dnsid))
     pckts = rdpcap(pcap)
     for p in pckts:
-        if p.haslayer(DNS):
-            if p.dns.id == id:
-                write(p)
+        if DNS in p:
+            #print("DNS packet found")
+            dns = p.getlayer(DNS)
+            if dns is not None:
+                #print(type(dns.id))
+                if dns.id == int(dnsid):
+                    print("Match found for {}".format(dns.id))
+                    write(p, dnsid)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pcap", help="traffic capture file")
+    parser.add_argument("-d", "--dnsid", help="dns query id")
+    args = parser.parse_args()
+
+    splitpcap(args.pcap, args.dnsid)
 
 if __name__ == '__main__':
-    def splitpcap()
+    main()
