4c6403f30716f3ed3447c605344a8c0731cf6364
Michael Ragusa <mragusa@users.noreply.github.com>
2024-09-24 01:27:39 -0400

    added argparse,  and cli flags, updated resolve_a with try/except

--- glue_verify.py
+++ glue_verify.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 
 import dns.resolver
+import argparse
 
 def resolve_a(hostname, nameserver):
     qname = dns.name.from_text(hostname)
+    print("Querying {} for {}".format(nameserver, hostname))
     q = dns.message.make_query(qname, dns.rdatatype.A)
-    r = dns.query.udp(q, nameserver )
-    a_rrset = r.find_rrset(r.answer, qname, dns.rdataclass.IN, dns.rdatatype.A)
+    r = dns.query.udp(q, nameserver,timeout=2)
+    try:
+        a_rrset = r.find_rrset(r.answer, qname, dns.rdataclass.IN, dns.rdatatype.A)
+    except Exception as e:
+        print("Error: {}".format(e))
     if a_rrset:
+        print("A_RRSet", a_rrset)
         for arr in a_rrset:
             return(arr.to_text())
+    #except (dns.exception.DNSException,dns.rcode.UnknownRcode,dns.exception.Timeout) as e:
+    #    print(e)
 
 def query_ns (domain, nameserver):
     qname = dns.name.from_text(domain)
     q = dns.message.make_query(qname, dns.rdatatype.NS)
-    #   print("The query is:")
-    #print(q)
-    #print("")
     r = dns.query.udp(q, nameserver )
-    #print("The response is:")
-    #print(r)
-    #print("")
-    #print("The nameservers are:")
     ns_rrset = r.find_rrset(r.answer, qname, dns.rdataclass.IN, dns.rdatatype.NS)
     if ns_rrset:
+        print("NS Query", ns_rrset)
         ns_results = {} 
         for rr in ns_rrset:
             ns_ip_address = resolve_a(rr.target.to_text(), nameserver)
@@ -32,8 +34,12 @@ def query_ns (domain, nameserver):
         return(ns_results)
 
 def main ():
-    domain = 'google.com'
-    nameserver = '8.8.8.8'
+    parser = argparse.ArgumentParser(description='Verify glue records for domains',)
+    parser.add_argument("-d", "--domain", help="Domain to verify")
+    parser.add_argument("-n", "--nameserver", help="Name server to begin tree")
+    args = parser.parse_args()
+    domain = args.domain
+    nameserver = args.nameserver
     print("Querying for {}".format(domain))
     domain_ns = query_ns(domain, nameserver)    
     glue_ns = {}
@@ -42,7 +48,7 @@ def main ():
     for dns in domain_ns:
         glue_ns_results = query_ns(domain, domain_ns[dns])
         glue_ns[dns] = glue_ns_results
-        #print(glue_ns)
+        print("Glue", glue_ns)
         if domain_ns == glue_ns[dns]:
             print("{} Glue Matches against {}".format(domain, dns))
             print(glue_ns[dns])
